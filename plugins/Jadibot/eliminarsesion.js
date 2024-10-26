44c1e73459072bd1a0c0025dd2434b07a5ced843
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-25 22:48:53 -0500

    Update eliminarsesion.js

--- plugins/Jadibot/eliminarsesion.js
+++ plugins/Jadibot/eliminarsesion.js
@@ -1,3 +1,4 @@
+//
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import path, { join } from 'path'
 let handler  = async (m, { conn }, args) => {
