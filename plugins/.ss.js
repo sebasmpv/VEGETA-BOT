3f6c84e12f54d2c83b65ee882756bcb1081d7ef7
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-26 00:29:14 -0500

    Update .ss.js

--- plugins/.ss.js
+++ plugins/.ss.js
@@ -1,56 +1,49 @@
-ñimport { sticker } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-let text1 = `✡︎ → Solicitado por:\n✡︎ → Bot:\n✡︎ → Made By:`
-let text2 = `✧ ${nombre}\n✧ Ai Otho\n✧ Daniel`
 let stiker = false
-try {
+let stick = args.join(" ").split("|");
+let f = stick[0] !== "" ? stick[0] : packname;
+let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+try { 	
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`☁️ *¡El video no puede durar mas de 8 segundos!*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('⚠️ Máximo 10 segundos')
 let img = await q.download?.()
-
-if (!img) return conn.reply(m.chat, `🍿 responda a un video o imagen`, m, rcanal)
-
+if (!img) return conn.reply(m.chat, `*Y LA IMAGEN? 🤔 RESPONDE A UNA IMAGEN, VIDEO, GIF PARA REALIZAR EL STICKERS USE* *${usedPrefix + command}*`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
 let out
 try {
-stiker = await sticker(img, false, text1, text2)
+stiker = await sticker(img, false, f, g)
 } catch (e) {
 console.error(e)
 } finally {
+ //await conn.reply(m.chat, `Calma crack estoy haciendo tu sticker 👏\n\n> *Recuerda los video son de 7 segundos*`, m)
 if (!stiker) {
 if (/webp/g.test(mime)) out = await webp2png(img)
 else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, text1, text2)
-}}
-} else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], text1, text2)
-
-else return m.reply(`💫 El url es incorrecto`)
-
-}
-} catch (e) {
+stiker = await sticker(false, out, f, g)
+}}} else if (args[0]) {
+if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+else return m.reply('URL invalido')
+}} catch (e) {
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `Yaemori Bot - MD 🚩`, body: `✡︎ Sticker By • YaemoriBot`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
-
-else return conn.reply(m.chat, '🍿 responde a un video o imagen', m, rcanal)
-
-
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: ``, mediaType: 2, sourceUrl: [nna, nn, md, yt].getRandom(), thumbnail: imagen4}}}, { quoted: m })
+else return conn.reply(m.chat, `*Y LA IMAGEN? 🤔 RESPONDE A UNA IMAGEN, VIDEO, GIF PARA REALIZAR EL STICKERS USE* *${usedPrefix + command}*`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
 }}
-handler.help = ['stiker <img>', 'sticker <url>']
+handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker', 'stiker']
-
+handler.command = ['s', 'sticker'] 
+handler.register = true
 export default handler
 
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
+return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
