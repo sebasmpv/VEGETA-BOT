37333a9de80b93ba37b753b2458d0c4ee2fa9b34
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-27 14:00:22 -0500

    𝙋𝙧𝙤𝙮𝙚𝙘𝙩𝙤𝙓/𝙀𝘽𝙂

--- plugins/Jadibot/eliminarsesion.js
+++ plugins/Jadibot/eliminarsesion.js
@@ -1,24 +1,34 @@
-//
+/*⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠ -- ⚠ PROHIBIDO EDITAR ⚠
+El codigo de este archivo fue realizado por:
+- ReyEndymion >> https://github.com/ReyEndymion
+*/
 
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import path, { join } from 'path'
-let handler  = async (m, { conn }, args) => {
+
+let handler  = async (m, { conn, usedPrefix, command}, args) => {
 let parentw = conn
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let uniqid = `${who.split`@`[0]}`
-if (global.conn.user.jid !== conn.user.jid) return conn.sendMessage(m.chat, {text: '*Use este comando directamente en el numero del Bot principal*'}, { quoted: m }) 
-else {
-await conn.sendMessage(m.chat, {text: "*👋🏻 Adiós Bot, haz dejado de ser un Bot*"}, { quoted: m }) 
-}
+let userS = `${conn.getName(who)}`
+    
+if (global.conn.user.jid !== conn.user.jid) {
+return conn.sendMessage(m.chat, {text: lenguajeGB.smsJBDel() + `\n\n*https://api.whatsapp.com/send/?phone=${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}&type=phone_number&app_absent=0*`}, { quoted: m }) 
+} else {
 try {
-fs.rmdir("./IgnaJadiBot/" + uniqid, { recursive: true, force: true })
-await conn.sendMessage(m.chat, {text : "*Todos los archivos de session fueron eliminados*" } , { quoted: m })
+await fs.rmdir("./GataJadiBot/" + uniqid, { recursive: true, force: true })
+await conn.sendMessage(m.chat, { text: lenguajeGB.smsJBAdios() }, { quoted: m })
+await conn.sendMessage(m.chat, { text : lenguajeGB.smsJBCerrarS() } , { quoted: m })
 } catch(err) {
-console.error('La carpeta o archivo de sesion no existen ', err)   
-}}
-handler.help = ['deletebot']
-handler.tags = ['jadibot']
-handler.command = /^(deletebot|eliminarsesion|deletesesion)$/i
+if (err.code === 'ENOENT' && err.path === `./GataJadiBot/${uniqid}`) {
+await conn.sendMessage(m.chat, { text: "Usted no es Sub Bot" }, { quoted: m })
+} else {
+console.error(userS + ' ' + lenguajeGB.smsJBErr(), err)
+}}}
+}
 
+handler.command = /^(deletesess?ion|eliminarsesion|bor|delsess?ion|cerrarsesion)$/i
+handler.private = false
 handler.fail = null
+
 export default handler
